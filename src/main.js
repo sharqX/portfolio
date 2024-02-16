3742bfd8e22dcf6a686ba208303ec9d3aea81efe
Jay Bhavsar <jbhv12@gmail.com>
2024-02-16 12:31:19 +0530

    added linkedin icon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
 
-import { faGithub, faTwitter, faDiscord, faJs, faVuejs, faGolang, faCss3, faNode } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faTwitter, faDiscord, faJs, faVuejs, faGolang, faCss3, faNode, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faAt, faDatabase, faBars, faLink, faEye, faCodeFork, faRss } from '@fortawesome/free-solid-svg-icons'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -11,7 +8,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default function (Vue) {
 
-  library.add(faGithub, faTwitter, faDiscord, faJs, faVuejs, faGolang, faCss3, faAt, faDatabase, faNode, faBars, faLink, faEye, faCodeFork, faRss)
+  library.add(faGithub, faTwitter, faDiscord, faJs, faVuejs, faGolang, faCss3, faNode, faLinkedin, faAt, faDatabase, faBars, faLink, faEye, faCodeFork, faRss)
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
