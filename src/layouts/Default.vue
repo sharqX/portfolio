6e143418bc9035b7891cbf1d79b8fcbae138d63f
Jay Bhavsar <jbhv12@gmail.com>
2024-04-10 11:29:54 +0530

    footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -21,8 +21,19 @@
     <slot />
 
     <footer class="contain flex justify-between mb-5 text-sl text-dimGreyAlt">
-      <p>Copyright © 2024 🚀</p>
-    </footer>
+    <p>
+        <span>&copy; 2024 🚀</span>
+    </p>
+    <p>
+        <a href="/privacy-policy.html">Privacy Policy</a> |
+        <a href="/terms-of-service.html">Terms of Service</a> |
+        <a href="/refund-policy.html">Refund Policy</a> |
+        <a href="/support.html">Support</a>
+    </p>
+</footer>
+
+</footer>
+
   </div>
 </template>
 
